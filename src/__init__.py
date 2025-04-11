6ba2d5ee2959d6d759e6a8a2b62763912750210a
Ignacio <81712119+Ignacioelamo@users.noreply.github.com>
2025-04-11 19:56:05 +0200

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -3,3 +3,11 @@ from .data.dataset_loader import DatasetLoader
 from .models.llm_handler import LLMHandler
 from .rephrasers.zero_shot_rephraser import ZeroShotRephraser
 from .rephrasers.few_shot_rephraser import FewShotRephraser
+
+__all__ = [
+    "Config",
+    "DatasetLoader",
+    "LLMHandler",
+    "ZeroShotRephraser",
+    "FewShotRephraser"
+]
